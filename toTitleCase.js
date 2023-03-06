d534d57985ca4d02dfdf79bcfc5f91809786db02
lesaprodan <99794388+lesaprodan@users.noreply.github.com>
2023-03-06 20:32:46 +0200

    Update toTitleCase.js

--- toTitleCase.js
+++ toTitleCase.js
@@ -18,7 +18,9 @@
 
 function toTitleCase(string) {
     // Put your code here
-}
+    return str.toLowerCase().split(' ').map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+}.join(' ');
   
 
 console.log('Running sanity checks for toTitleCase:');
